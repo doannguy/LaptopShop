c37a8f3fa8988bfd6a7df4d1d322c956d7b211de
Hai Tran <you@example.com>
2024-06-04 14:19:02 +0700

    update

--- BE/routes/web.php
+++ BE/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('dashboard');
+})->name('dashboard')->middleware('auth');
 
 
 include "auth.php";

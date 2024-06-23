51f999eb10445dd74b87d0c4da0b03b7bd41ec58
Hoàng Đức <ductrieuhoang@gmail.com>
2024-06-23 15:32:14 +0700

    Update

--- FE/src/components/widget/Loading.jsx
+++ FE/src/components/widget/Loading.jsx
@@ -3,7 +3,7 @@ import ReactLoading from 'react-loading';
 
 const Loading = () => {
     const [showLoading, setShowLoading] = useState(true);
-    
+
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowLoading(true);
@@ -13,9 +13,9 @@ const Loading = () => {
 
     if (!showLoading) {
         return (
-            <div style={{ 
+            <div style={{
                 color: 'red',
-             }}>
+            }}>
                 <h3>Không kết nối internet</h3>
                 <p>Vui lòng kiểm tra lại kết nối internet</p>
             </div>
@@ -23,21 +23,21 @@ const Loading = () => {
     }
 
     return (
-        <div style={{ 
-            position: 'fixed', 
-            top: 0, 
-            right: 0, 
-            bottom: 0, 
-            left: 0, 
+        <div style={{
+            position: 'fixed',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
             zIndex: 99999999,
             display: 'flex',
             backgroundColor: 'rgba(255, 255, 255)',
             justifyContent: 'center',
             alignItems: 'center',
         }}>
-            <ReactLoading height={'5%'} width={'5%'} type="spokes" color="#3572EF" />
+            <ReactLoading type={'spin'} height={'5%'} width={'5%'} color="#3572EF" />
         </div>
     );
 }
- 
+
 export default Loading;

8737a592d81d9d8f5af5620cb684bc6d6d126f16
Tran Van Hai <82074781+HaiTiger23@users.noreply.github.com>
2024-07-08 14:47:48 +0700

    Merge branch 'live'

--- BE/app/Http/Requests/Admin/Order/UpdateOrderRequest.php
+++ BE/app/Http/Requests/Admin/Order/UpdateOrderRequest.php
@@ -26,7 +26,7 @@ class UpdateOrderRequest extends FormRequest
     {
         return [
             'id' => 'required|exists:orders,id',
-            'status' => ['required|in:' . Order::STATUS_SUCCESS . ',' . Order::STATUS_WAITING . ',' . Order::STATUS_CANCEL . ',' . Order::STATUS_CONFIRM . ',' . Order::STATUS_SHIPPING, Order::STATUS_WAITING_PAYMENT],
+            'status' => 'required|in:' . Order::STATUS_SUCCESS . ',' . Order::STATUS_WAITING . ',' . Order::STATUS_CANCEL . ',' . Order::STATUS_CONFIRM . ',' . Order::STATUS_SHIPPING.','.Order::STATUS_WAITING_PAYMENT,
             'note' => 'nullable|string|max:255',
         ];
     }
